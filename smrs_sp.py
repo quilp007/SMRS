39eae0f6ec17c30c46c08dad9a735ef2707b8903
quilp007 <quilp007@gmail.com>
2023-03-07 23:19:42 +0900

    modify web_app

--- smrs_sp.py
+++ smrs_sp.py
@@ -1,6 +1,7 @@
 import gradio as gr
 import SMRS
 import json
+import time
 
 title = "Multiple Interfaces"
 
@@ -15,6 +16,15 @@ g_heat_on_time          = 0
 g_road_temp             = 0
 g_road_humidity         = 0
 g_air_temp              = 0
+
+g_l_pre_heat            = 0
+g_l_heat                = 0
+g_l_emc_heat            = 0
+
+label_dict = {  'label_pre_heat_on': 'gray',
+                'label_heat_on': 'gray',
+                'label_emc_heat_on': 'gray',
+                'btn_AUTO_MODE': 'gray'}
 
 # global is_login
 # is_login = 0
@@ -69,6 +79,19 @@ def fn_road_humidity():
 def fn_air_temp():
     return g_air_temp
 
+def fn_update_label_1():
+    return gr.Label.update(color=label_dict['label_pre_heat_on'])
+
+def fn_update_label_2():
+    return gr.Label.update(color=label_dict['label_heat_on'])
+
+def fn_update_label_3():
+    return gr.Label.update(color=label_dict['label_emc_heat_on'])
+
+def fn_update_label_4():
+    return gr.Label.update(color=label_dict['btn_AUTO_MODE'])
+
+
 app1 = gr.Interface(fn=dummy, inputs=None, outputs="text")
 app2 = gr.Interface(fn=dummy, inputs=None, outputs="text")
 app3 = gr.Interface(fn=dummy, inputs=None, outputs="text")
@@ -94,64 +117,62 @@ with gr.Blocks() as app1:
     with gr.Row():
         gr.Markdown("현재 상태")
     with gr.Row():
-        gr.Box()
-        gr.Box()
-        gr.Box()
+        label_4 = gr.Label("자동")
+        label_1 = gr.Label("예열 가동 중")
+        label_2 = gr.Label("본 가동 중")
+        label_3 = gr.Label("비상 가동 중")
 
-    app1.load(fn_road_temp,             inpupts=None, outputs=cur_road_temp,        every=1)
-    app1.load(fn_road_humidity,         inpupts=None, outputs=cur_road_hum,         every=1)
-    app1.load(fn_air_temp,              inpupts=None, outputs=cur_air_temp,         every=1)
+    app1.load(fn_road_temp,             inputs=None, outputs=cur_road_temp,         every=1)
+    app1.load(fn_road_humidity,         inputs=None, outputs=cur_road_hum,          every=1)
+    app1.load(fn_air_temp,              inputs=None, outputs=cur_air_temp,          every=1)
 
-    app1.load(fn_pre_heat_set_temp,     inpupts=None, outputs=pre_heat_set_temp,    every=1)
-    app1.load(fn_pos_heat_set_temp,     inpupts=None, outputs=pos_heat_set_temp,    every=1)
-    app1.load(fn_pos_heat_set_hum,      inpupts=None, outputs=pos_heat_set_hum,     every=1)
-    app1.load(fn_emer_heat_set_temp,    inpupts=None, outputs=emer_heat_set_temp,   every=1)
+    app1.load(fn_pre_heat_set_temp,     inputs=None, outputs=pre_heat_set_temp,     every=1)
+    app1.load(fn_pos_heat_set_temp,     inputs=None, outputs=pos_heat_set_temp,     every=1)
+    app1.load(fn_pos_heat_set_hum,      inputs=None, outputs=pos_heat_set_hum,      every=1)
+    app1.load(fn_emer_heat_set_temp,    inputs=None, outputs=emer_heat_set_temp,    every=1)
 
+    app1.load(fn_update_label_1,        inputs=None, outputs=label_1,               every=1)
+    app1.load(fn_update_label_2,        inputs=None, outputs=label_2,               every=1)
+    app1.load(fn_update_label_3,        inputs=None, outputs=label_3,               every=1)
+    app1.load(fn_update_label_4,        inputs=None, outputs=label_4,               every=1)
 
 def update_value(val):
     return f'Value is set to {val}'
 
-def pre_heat():
-    widget.send_CMD(widget.btn_PRE_HEAT_ON)
-
-    return f'pre_heat'
-
 
 def pre_heat_end():
-    return f'pre_heat_end'
+    widget.pressed_button(widget.btn_PRE_HEAT_ON_AND_STOP)
+    return f'예열 가동 후 정지'
 
 
 def pre_heat_auto_change():
-    return f'pre_heat_auto_change'
+    widget.pressed_button(widget.btn_PRE_HEAT_ON)
+    return f'예열 가동 후 자동 전환'
 
 
 def pos_heat_end():
-    return f'pos_heat_end'
+    widget.pressed_button(widget.btn_HEAT_ON_AND_STOP)
+    return f'본 가동 후 정지'
 
 
 def pos_heat_auto_change():
-    return f'pos_heat_auto_change'
-
-
-def pos_heat():
-    widget.send_CMD(widget.btn_HEAT_ON)
-    return f'pos_heat'
+    widget.pressed_button(widget.btn_HEAT_ON)
+    return f'본 가동 후 자동 전환'
 
 
 def auto():
-    return f'auto'
-
-
-def man():
-    return f'man'
+    widget.pressed_button(widget.btn_AUTO_MODE)
+    return f'자동'
 
 
 def end():
-    return f'end'
+    widget.pressed_button(widget.btn_HEAT_STOP)
+    return f'정지'
 
 
 def init_set():
-    return f'init_set'
+    widget.pressed_button(widget.btn_INIT_MODE)
+    return f'설정 초기화'
 
 
 with gr.Blocks() as app2:
@@ -163,58 +184,53 @@ with gr.Blocks() as app2:
                 with gr.Column(scale=1, min_width=70):
                     l_pre_heat_set_temp = gr.Textbox(label="")
                 with gr.Column(scale=10):
-                    pre_heat_set_temp = gr.Slider(0, 10, step=1, label="[설정] 예열 설정 노면 온도")
+                    s_pre_heat_set_temp = gr.Slider(0, 10, step=1, label="[설정] 예열 설정 노면 온도")
                 app2.load(fn_pre_heat_set_temp,  inpupts=None, outputs=l_pre_heat_set_temp,  every=1)
 
             with gr.Row():
                 with gr.Column(scale=1, min_width=70):
                     l_pre_heat_dur_time = gr.Textbox(label="")
                 with gr.Column(scale=10):
-                    pre_heat_dur_time = gr.Slider(0, 6, step=1, label="[설정] 예열 가동 지속 횟수 (회)")
+                    s_pre_heat_dur_time = gr.Slider(0, 6, step=1, label="[설정] 예열 가동 지속 횟수 (회)")
                 app2.load(fn_pre_heat_dur_time,  inpupts=None, outputs=l_pre_heat_dur_time,  every=1)
 
             with gr.Row():
                 with gr.Column(scale=1, min_width=70):
                     l_pos_heat_set_temp = gr.Textbox(label="")
                 with gr.Column(scale=10):
-                    pos_heat_set_temp = gr.Slider(-10, 10, step=1, label="[설정] 본 가동 설정 노면 온도 (℃)")
+                    s_pos_heat_set_temp = gr.Slider(-10, 10, step=1, label="[설정] 본 가동 설정 노면 온도 (℃)")
                 app2.load(fn_pos_heat_set_temp,  inpupts=None, outputs=l_pos_heat_set_temp,  every=1)
 
             with gr.Row():
                 with gr.Column(scale=1, min_width=70):
                     l_pos_heat_set_hum = gr.Textbox(label="")
                 with gr.Column(scale=10):
-                    pos_heat_set_hum = gr.Slider(0, 9, step=1, label="[설정] 본 가동 설정 노면 습도 (Lev)")
+                    s_pos_heat_set_hum = gr.Slider(0, 9, step=1, label="[설정] 본 가동 설정 노면 습도 (Lev)")
                 app2.load(fn_pos_heat_set_hum,  inpupts=None, outputs=l_pos_heat_set_hum,  every=1)
 
             with gr.Row():
                 with gr.Column(scale=1, min_width=70):
                     l_pos_heat_dur_time = gr.Textbox(label="")
                 with gr.Column(scale=10):
-                    pos_heat_dur_time = gr.Slider(0, 4, step=1, label="[설정] 본 가동 지속 횟수 (회)")
+                    s_pos_heat_dur_time = gr.Slider(0, 4, step=1, label="[설정] 본 가동 지속 횟수 (회)")
                 app2.load(fn_pos_heat_dur_time,  inpupts=None, outputs=l_pos_heat_dur_time,  every=1)
 
             with gr.Row():
                 with gr.Column(scale=1, min_width=70):
                     l_emer_heat_set_temp = gr.Textbox(label="")
                 with gr.Column(scale=10):
-                    emer_heat_set_temp = gr.Slider(-30, -20, step=1, label="비상 가동 대기 온도(℃)")
+                    s_emer_heat_set_temp = gr.Slider(-30, -20, step=1, label="비상 가동 대기 온도(℃)")
                 app2.load(fn_emer_heat_set_temp,  inpupts=None, outputs=l_emer_heat_set_temp,  every=1)
 
             md = gr.Markdown()
 
-            pre_heat_set_temp.change(fn=update_pre_heat_set_temp,  inputs=pre_heat_set_temp,  outputs=None)
-            pre_heat_dur_time.change(fn=update_pre_heat_dur_time,  inputs=pre_heat_dur_time,  outputs=None)
-            pos_heat_set_temp.change(fn=update_pos_heat_set_temp,  inputs=pos_heat_set_temp,  outputs=None)
-            pos_heat_set_hum.change(fn=update_pos_heat_set_hum,    inputs=pos_heat_set_hum,   outputs=None)
-            pos_heat_dur_time.change(fn=update_pos_heat_dur_time,  inputs=pos_heat_dur_time,  outputs=None)
-            emer_heat_set_temp.change(fn=update_emer_heat_set_temp,inputs=emer_heat_set_temp, outputs=None)
+            s_pre_heat_set_temp.change(fn=update_pre_heat_set_temp,  inputs=s_pre_heat_set_temp,  outputs=None)
+            s_pre_heat_dur_time.change(fn=update_pre_heat_dur_time,  inputs=s_pre_heat_dur_time,  outputs=None)
+            s_pos_heat_set_temp.change(fn=update_pos_heat_set_temp,  inputs=s_pos_heat_set_temp,  outputs=None)
+            s_pos_heat_set_hum.change(fn=update_pos_heat_set_hum,    inputs=s_pos_heat_set_hum,   outputs=None)
+            s_pos_heat_dur_time.change(fn=update_pos_heat_dur_time,  inputs=s_pos_heat_dur_time,  outputs=None)
+            s_emer_heat_set_temp.change(fn=update_emer_heat_set_temp,inputs=s_emer_heat_set_temp, outputs=None)
 
-            pre_heat_btn = gr.Button("강제 예열 가동")
-            pos_heat_btn = gr.Button("강제 본 가동")
-
-            pre_heat_btn.click(fn=pre_heat, inputs=None, outputs=md)
-            pos_heat_btn.click(fn=pos_heat, inputs=None, outputs=md)
 
         with gr.Column():
             gr.Markdown("수동 설정")
@@ -233,11 +249,8 @@ with gr.Blocks() as app2:
     with gr.Row():
         auto_btn = gr.Button("자동")
         auto_btn.click(fn=auto, inputs=None, outputs=md)
-        man_btn = gr.Button("수동")
-        man_btn.click(fn=man, inputs=None, outputs=md)
         end_btn = gr.Button("정지")
         end_btn.click(fn=end, inputs=None, outputs=md)
-    with gr.Row():
         init_set_btn = gr.Button("설정초기화")
         init_set_btn.click(fn=init_set, inputs=None, outputs=md)
 
@@ -280,8 +293,10 @@ with gr.Blocks() as app4:
 #         login.close()
 #         break
 def check_auth_mongodb(username, password):
+    SMRS.DEVICE_ID = username
+    SMRS.initMongoDB()
     global LOGIN_FLAG
-    saved_passwd = SMRS.signup_col.find_one({'id': username})['pwd']
+    saved_passwd = SMRS.mongodb_signup_col.find_one({'id': username})['pwd']
 
     if saved_passwd == password:
         print('password is correct!!')
@@ -314,48 +329,15 @@ def on_message_sp(msg, topic):
     global g_road_temp     
     global g_road_humidity 
     global g_air_temp     
+    global g_l_pre_heat
 
     jsonData = json.loads(msg)
     print('on_message_callback: ', msg)
-    if topic == SMRS.sub_root_topic + 'STATUS':
-        print("CMD: ", "CH1: ", str(jsonData['CH1']))
-        print("CMD: ", "CH2: ", str(jsonData['CH2']))
 
-        if jsonData['CH1'] == True and jsonData[ 'CH2'] == True:
-            return
-
-        if jsonData['CH1'] == True:  # PRE HEAT ON
-            print("PRE HEAT: ON")
-            #TODO: '강제 예열 가동' 버튼 -> YELLOW color
-        elif jsonData['CH1'] == False:  # PRE HEAT OFF
-            print("PRE HEAT: OFF")
-            #TODO: '강제 예열 가동' 버튼 -> GRAY color
-
-        if jsonData['CH2'] == True:  # HEAT ON
-            print("HEAT: ON")
-            #TODO: '강제 본 가동' 버튼 -> PINK color
-        elif jsonData['CH2'] == False:  # HEAT OFF
-            print("HEAT: OFF")
-            #TODO: '강제 본 가동' 버튼 -> GRAY color
-
-        if jsonData['CH1'] == False and jsonData[ 'CH2'] == False:
-            print("PRE HEAT: OFF")
-            print("HEAT: OFF")
-            # self.flag_HEAT_ON = False
-
-        """
-        self.config_dict = {
-            'pre_heat_road_temp':   3,
-            'heat_road_temp':       1,
-            'set_road_humidity':    3,
-            'set_air_temp':         -15,
-            'pre_heat_on_time':     60,
-            'heat_on_time':         90,
-            'road_temp':            0,
-            'road_humidity':        0,
-            'air_temp':             0
-        }
-        """
+    if topic == SMRS.sub_root_topic + 'DATA':
+        roadTemp = jsonData['road_temp']
+        roadHumidity = jsonData['road_humidity']
+        airTemp = jsonData['air_temp']
 
     elif topic == SMRS.sub_root_topic + 'CONFIG':
         print('received CONFIG')
@@ -382,6 +364,68 @@ def on_message_sp(msg, topic):
 
     elif topic == SMRS.sub_root_topic + 'IMAGE':
         print('image captured')
+        filename = jsonData['filename'].split('/')[2]
+        img_str = jsonData['IMG']
+
+        # to decode back to np.array
+        # jpg_original = base64.b64decode(img_str)
+        # jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+        # decoded_img = cv2.imdecode(jpg_as_np, flags=1)
+
+        # filename = jsonData['filename']
+        # cv2.imwrite(filename, decoded_img)
+        # print('image saved')
+
+        # self.update_image(decoded_img, 480, 360)
+        # self.btn_capture.setStyleSheet("background-color: gray; border: 1px solid black")
+        # self.textEdit.append('captured ' + filename)
+
+    elif topic == SMRS.sub_root_topic + 'BUTTON':
+        print(jsonData['btn'][0][4:], ': ', jsonData['btn'][1])
+        if jsonData['btn'][1] != '#808080' and jsonData['btn'][1] != 'gray':
+        # if jsonData['btn'][1] in ['blue', 'pink', 'yellow']:
+            time_text = time.strftime('%y.%m.%d_%H:%M:%S', time.localtime(time.time()))
+            log_text = time_text + '   ' + jsonData['btn'][0][4:]
+            # self.textEdit_log.append(log_text)
+            # TODO: write log
+            print(log_text)
+        
+        if jsonData['btn'][0] == 'btn_AUTO_MODE':
+            label_dict[jsonData['btn'][0]] = jsonData['btn'][1]
+
+        # self.findChild(QPushButton, jsonData['btn'][0]).setStyleSheet("background-color: {}".format(jsonData['btn'][1]))
+        # TODO: change button color
+
+    elif topic == SMRS.sub_root_topic + 'LABEL':
+        # self.findChild(QLabel, jsonData['label'][0]).setStyleSheet("background-color: {}".format(jsonData['label'][1]))
+        # TODO: change label color
+        print(jsonData['label'][0][6:], ': ', jsonData['label'][1])
+        label_dict[jsonData['label'][0]] = jsonData['label'][1]
+
+        if jsonData['label'][0] == 'label_emc_heat_on':
+            time_text = time.strftime('%y.%m.%d_%H:%M:%S', time.localtime(time.time()))
+            log_text = time_text + '   ' + 'EMC_HEAT_ON'
+            # self.textEdit_log.append(log_text)
+            # TODO: write log
+            print(log_text)
+            
+        
+
+        """
+        self.config_dict = {
+            'pre_heat_road_temp':   3,
+            'heat_road_temp':       1,
+            'set_road_humidity':    3,
+            'set_air_temp':         -15,
+            'pre_heat_on_time':     60,
+            'heat_on_time':         90,
+            'road_temp':            0,
+            'road_humidity':        0,
+            'air_temp':             0
+        }
+        """
+
+
 
 widget = SMRS.run(pc_app = False)
 # password = widget.util_func.read_var('enerpia_1')
