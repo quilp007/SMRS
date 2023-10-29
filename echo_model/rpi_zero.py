c4b65171d0dbe731f423e167052c92f71fa0eb5a
quilp007 <quilp007@gmail.com>
2023-10-29 11:10:52 +0900

    modify rpi_zero for PC code

--- echo_model/rpi_zero.py
+++ echo_model/rpi_zero.py
@@ -1,214 +1,185 @@
-import sys, os, serial, time, datetime
-import serial.tools.list_ports
+import sys, os, time, datetime
 from PyQt5.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox #, QProgressBar,  QComboBox, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QApplication 
 from PyQt5.uic import loadUi
-from PyQt5 import uic, QtTest
+from PyQt5 import uic, QtTest, QtCore
 
 import zmq
-context = zmq.Context()
 
-socket = context.socket(zmq.PUB)
+context = zmq.Context()
+socket = context.socket(zmq.PAIR)
 socket.bind("tcp://*:5555")
 
-DEBUG_RECEIVE_TEST_DATA = True
-PASS_CHECKSUM = True
+AUTO_BUTTON     = 0
+MANUAL_BUTTON   = 1
+LIVE            = 2
+SOCKET_IDX      = 3
 
-PORTS = []
+ON  = True
+OFF = False
 
-if not DEBUG_RECEIVE_TEST_DATA:
-    PORTS = [
-        p.device
-        for p in serial.tools.list_ports.comports()
-        # if 'USB' in p.description
-    ]
-    print(PORTS)
+command_dict_list = []
 
-# PORTS = ["/dev/serial0", "/dev/serial1", "/dev/ttyUSB0"]
-
-data_arr = [
-    # [0x12, 0x00, 0x21, 0xcd],
-    [0x12, 0x00, 0x20, 0xcd],
-    [0x12, 0x08, 0x61, 0xcd],
-    [0x12, 0x91, 0xA1, 0xcc],
-    [0x12, 0x62, 0x21, 0xcc],
-    [0x12, 0x03, 0x20, 0xcc],
-    [0x12, 0x84, 0x61, 0xcc],
-    [0x12, 0x44, 0xA1, 0xcc]
-]
-
-data_arr2 = [
-    [b'\x12\x00\x20\xcd'],
-    [b'\x12\x08\x61\xcd'],
-    [b'\x12\x91\xA1\xcd'],
-    [b'\x12\x62\x21\xcd'],
-    [b'\x12\x03\x20\xcd'],
-    [b'\x12\x84\x61\xcd'],
-    [b'\x12\x44\xA1\xcd']
-]
-
-DATA_2 = {
-    'radar_1':      7,
-    'radar_0':      6,
-    'elec_lock':    5,
-    'working_sw':   4
-}
-
-RADAR_1         = 7
-RADAR_0         = 6
-FOOT_SW         = 5
-ELEC_LOCK       = 4
-WORKING_SW      = 3
-SHOT_MODE_2     = 2
-SHOT_MODE_1     = 1
-SHOT_MODE_0     = 0
-
-SHOT_SW         = 7
-ABNORMAL_SW     = 6
-ELEC            = 5
-
-
-vulcan_data = {
-    'source':                       0xd1,       # 0xd1, 0xd2, 0xd3, 0xd4
-    'destination':                  0x11,       # 0x11, 0x21, 0x31, 0x41
-    'phase':                        0xa1,       # 0xa0, 0xa1, 0xa2
-    'gun_barrel_elevation_angle':   0,          # 0 ~ 90
-    'gun_azimuth_angle':            -90,          # -180 ~ 180
-    'scope_elevation_angle':        0,          # 0 ~ 90
-    'scope_azimuth_angle':          -90,          # -180 ~ 180
-    'trigger':                      0,          # 0 or 1
-    'shot_motor':                   0,          # ?
-    'gun_barrel_speed_up_down':     0,          # ?
-    'gun_barrel_speed_left_right':  0           # ?
-}
-
-def twoComplement(number):
-    return (-number) & (2**8 - 1)
+for idx in range(8):
+    command_dict = {
+        'alive':        False,
+        'auto_mode':    OFF,
+        'manual_mode':  OFF
+    }
+    command_dict_list.append(command_dict)
 
 #--------------------------------------------------------------
-# [THREAD] RECEIVE from FPGA, SEND to Smart Glass
+# [THREAD] RECEIVE from RPI Zero
 #--------------------------------------------------------------
-class THREAD_FPGA_SMART_GLASS(QThread):
-    vulcan_qt_signal = pyqtSignal(dict)
-    def __init__(self):
-        super(THREAD_FPGA_SMART_GLASS, self).__init__()
-        self.__suspend = False
-        self.__exit = False
-        self.idx = 0
+class THREAD_RECEIVE_MSG(QThread):
+    received_alive = pyqtSignal(int, dict)
+    def __init__(self, idx, qt_obj):
+        super( THREAD_RECEIVE_MSG, self).__init__()
+        self.obj = qt_obj
+        self.event_list = []
+        self.idx = idx
 
-    def _test_data(self):
-        # self.idx += 1
+    def run(self):
+        while True:
+            self.event_list = []
 
-        for key in vulcan_data:
-            if key in ['source', 'destination', 'phase']: continue
-            if self.idx > 40:
-                vulcan_data[key] = 0
+            event = self.obj.button_list[self.idx][SOCKET_IDX].poll(timeout = 2000) # wait 1 seconds
+            self.check_event2(self.idx, event)
 
-            vulcan_data[key] += (self.idx * 2)
-            self.idx += 1
+    def check_event2(self, idx, event):
+        if event == 0:
+            print(f'[{idx}]', 'timeout!!!!!!!!!!!!!!!!!!')
+            command_dict_list[idx]['alive'] = False
+            self.received_alive.emit(idx, command_dict_list[idx])
+            return
+        else:
+            # print(f'[{idx}]', 'received alive!!')
+            try:
+                message = self.obj.button_list[idx][SOCKET_IDX].recv_json()
+            except:
+                print('rcv error!!')
+                return
 
-            print(key, vulcan_data[key])
-            print('idx: ', self.idx)
-
-        if self.idx > 50: self.idx = 0
-
-    def _test_data2(self):
-        if self.idx == 100: self.idx = 0
-        self.idx += 1
-        print('[idx] : ', self.idx)
-
-        for key in vulcan_data:
-            if key in ['source', 'destination', 'phase']: continue
-            if self.idx == 100:
-                vulcan_data[key] = 0
-
-            if 'elevation_angle' in key:
-                vulcan_data[key] += 90/100
-            elif 'azimuth_angle' in key:
-                vulcan_data[key] += 180/100
-            elif  'trigger' in key:
-                if self.idx > 50: vulcan_data[key] = 1
-                else: vulcan_data[key] = 0
+            if message['alive']:
+                print(f'[THREAD] {[idx]} [alive]', message)
             else:
-                vulcan_data[key] = self.idx
+                print('[THREAD] not alive', message)
 
-            print(key, vulcan_data[key])
+            self.received_alive.emit(idx, message)
 
-    def _uartHandler(self, rcvData):
-        print(['0x{:02x}'.format(i) for i in rcvData])
-
-    # --- RECEIVE UART DATA and SEND by ZMQ
-    def run(self):
-        while True:
-            ### Suspend ###
-            while self.__suspend:
-                time.sleep(0.5)
-
-
-            self._test_data2()
-            self.vulcan_qt_signal.emit(vulcan_data)
-
-            socket.send_json(vulcan_data)
-
-            QtTest.QTest.qWait(1000)
-            
-            ### Exit ###
-            if self.__exit:
-                break
-
-    def mySuspend(self):
-        self.__suspend = True
-         
-    def myResume(self):
-        self.__suspend = False
-
-    def myExit(self):
-        self.__exit = True
-
-#--------------------------------------------------------------
-# [THREAD] RECEIVE from MCU
-#--------------------------------------------------------------
-class THREAD_RECEIVE_MCU(QThread):
-    def __init__(self):
-        super( THREAD_RECEIVE_MCU, self).__init__()
-        self.__suspend = False
-        self.__exit = False
-
-
-    # --- RECEIVE MCU UART DATA
-    def run(self):
-        while True:
-            ### Suspend ###
-            while self.__suspend:
-                time.sleep(0.5)
-
-            
-            ### Exit ###
-            if self.__exit:
-                break
-
-    def mySuspend(self):
-        self.__suspend = True
-         
-    def myResume(self):
-        self.__suspend = False
-
-    def myExit(self):
-        self.__exit = True
 
 class main_window(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         loadUi('./rpi_zero.ui', self)        
-        
+
         # setting  the fixed width of window       
-        # width = 1000
+        # widtr = 1000
         # height = 700
         # self.setFixedWidth(width)
         # self.setFixedHeight(height)
 
-        self.pushButton_8.setStyleSheet("border-style: outset; border-width: 0px")
-        self.pushButton_9.setStyleSheet("border-style: outset; border-width: 0px")
+        self.button_list = [
+            #         0                     1                   2               3
+            # AUTO MODE BUTTON   # MANUAL MODE BUTTON  # CONNECTION BUTTON   # SOCKET
+            [self.pb_autoMode_0, self.pb_manualMode_0, self.pb_connection_0],
+            [self.pb_autoMode_1, self.pb_manualMode_1, self.pb_connection_1],
+            [self.pb_autoMode_2, self.pb_manualMode_2, self.pb_connection_2],
+            [self.pb_autoMode_3, self.pb_manualMode_3, self.pb_connection_3],
+            [self.pb_autoMode_4, self.pb_manualMode_4, self.pb_connection_4],
+            [self.pb_autoMode_5, self.pb_manualMode_5, self.pb_connection_5],
+            [self.pb_autoMode_6, self.pb_manualMode_6, self.pb_connection_6],
+            [self.pb_autoMode_7, self.pb_manualMode_7, self.pb_connection_7]
+        ]
+
+        for buttons in self.button_list:
+            for button in buttons:
+                button.setStyleSheet("border-style: outset; border-width: 0px")
+                if 'connection' in button.objectName(): continue
+                button.setDisabled(True)
+
+            buttons[AUTO_BUTTON].clicked.connect(self.func_pb_autoMode)
+            buttons[MANUAL_BUTTON].clicked.connect(self.func_pb_manualMode)
+
+        for idx in range(8):
+            sock = context.socket(zmq.PAIR)
+            sock.bind(f"tcp://*:550{idx}")
+            self.button_list[idx].append(sock)  # zmq 소켓을 각 button_list 마지막에 넣는다.
+            print('idx: ', idx, 'socket binded!!')
+
+        for idx in range(8):
+            thread = THREAD_RECEIVE_MSG(idx, self)
+            thread.received_alive.connect(self.alive_func)
+            thread.start()
+            print('idx: ', idx, 'thread started!!')
+            time.sleep(0.1)
+
+    def alive_func(self, idx, message):
+        if message['alive']:
+            # print('+++ idx: ',idx, 'alive', message)
+            self.button_list[idx][LIVE].setChecked(True)
+            self.button_list[idx][AUTO_BUTTON].setEnabled(True)
+            # self.pb_manualMode_1.setEnabled(True)
+        else:
+            print('--- not alive')
+            self.button_list[idx][LIVE].setChecked(False)
+            self.button_list[idx][AUTO_BUTTON].setChecked(False)
+            self.button_list[idx][MANUAL_BUTTON].setChecked(False)
+            self.button_list[idx][AUTO_BUTTON].setDisabled(True)
+            self.button_list[idx][MANUAL_BUTTON].setDisabled(True)
+            return
+        
+        if message['auto_mode']:
+            self.button_list[idx][AUTO_BUTTON].setChecked(True)
+            self.button_list[idx][MANUAL_BUTTON].setAutoExclusive(False)
+        else:
+            self.button_list[idx][AUTO_BUTTON].setChecked(False)
+            self.button_list[idx][MANUAL_BUTTON].setAutoExclusive(True)
+
+            if message['manual_mode']:
+                self.button_list[idx][MANUAL_BUTTON].setChecked(True)
+            else:
+                self.button_list[idx][MANUAL_BUTTON].setChecked(False)
+
+    def func_pb_autoMode(self):
+        sender = self.sender()  # 이벤트를 발생시킨 버튼을 찾습니다.
+        if sender:
+            print(f'{sender.objectName()} 버튼이 클릭되었습니다.')
+
+        num = int(sender.objectName()[-1])
+        print('num: ', num)
+
+        self.button_list[num][MANUAL_BUTTON].setChecked(False)
+
+        if sender.isChecked():
+            command_dict_list[num]['auto_mode'] = ON
+            command_dict_list[num]['manual_mode'] = OFF
+            print("auto mode pressed.")
+            self.button_list[num][MANUAL_BUTTON].setDisabled(True)
+        else:
+            command_dict_list[num]['auto_mode'] = OFF
+            print("auto mode not pressed.")
+            self.button_list[num][MANUAL_BUTTON].setEnabled(True)
+
+        self.button_list[num][SOCKET_IDX].send_json(command_dict_list[num])
+        print('[{}][{}] {}'.format(sys._getframe().f_code.co_name, num, command_dict_list[num]))
+        
+    def func_pb_manualMode(self):
+        sender = self.sender()  # 이벤트를 발생시킨 버튼을 찾습니다.
+        if sender:
+            print(f'{sender.objectName()} 버튼이 클릭되었습니다.')
+
+        num = int(sender.objectName()[-1])
+        print('num: ', num)
+
+        if sender.isChecked():
+            command_dict_list[num]['manual_mode'] = ON
+            print("manual mode pressed.")
+        else:
+            command_dict_list[num]['manual_mode'] = OFF
+            print("manual mode not pressed.")
+
+        self.button_list[num][SOCKET_IDX].send_json(command_dict_list[num])
 
 
 def run():
