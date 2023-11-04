ba8f18da9cd056f84d563743fe7f841f0ce8bc13
quilp007 <quilp007@gmail.com>
2023-11-04 15:22:08 +0900

    - add GPIO option (for not RPI board) - add connection timeout and wait

--- echo_model/rpi_zero.py
+++ echo_model/rpi_zero.py
@@ -1,15 +1,21 @@
 import time
 import zmq
-import RPi.GPIO as GPIO
 import threading as th
 import atexit
+
+RPI_ZERO = True
+
+if RPI_ZERO:
+    import RPi.GPIO as GPIO
+
 
 ON  = True
 OFF = False
 
 # GPIO Setting
-print(GPIO.RPI_INFO)
-GPIO.setmode(GPIO.BOARD)
+if RPI_ZERO:
+    print(GPIO.RPI_INFO)
+    GPIO.setmode(GPIO.BOARD)
 
 # I2C pin for ADC [TODO]
 ADC_SDA                 = 3
@@ -31,34 +37,50 @@ ADDR_0_PIN              = 15
 ADDR_1_PIN              = 36
 ADDR_2_PIN              = 37
 
-
-GPIO.setup(AUTO_MODE_RELAY_PIN, GPIO.OUT)
-GPIO.setup(MANUAL_MODE_RELAY_PIN, GPIO.OUT)
-
-GPIO.setup(LED_AUTO_MODE_ON  , GPIO.OUT)
-GPIO.setup(LED_MANUAL_MODE_ON, GPIO.OUT)
-GPIO.setup(LED_MANUAL_OUT_ON , GPIO.OUT)
-GPIO.setup(LED_MANUAL_OUT_OFF, GPIO.OUT)
-GPIO.setup(LED_WIFI_LINK     , GPIO.OUT)
-
-GPIO.setup(ADDR_0_PIN, GPIO.IN)
-GPIO.setup(ADDR_1_PIN, GPIO.IN)
-GPIO.setup(ADDR_2_PIN, GPIO.IN)
-
 addr = 0
-if GPIO.input(ADDR_2_PIN):
-    addr += 1 << 2
-if GPIO.input(ADDR_1_PIN):
-    addr += 1 << 1
-if GPIO.input(ADDR_0_PIN):
-    addr += 1
+
+if RPI_ZERO:
+    GPIO.setup(AUTO_MODE_RELAY_PIN, GPIO.OUT)
+    GPIO.setup(MANUAL_MODE_RELAY_PIN, GPIO.OUT)
+
+    GPIO.setup(LED_AUTO_MODE_ON  , GPIO.OUT)
+    GPIO.setup(LED_MANUAL_MODE_ON, GPIO.OUT)
+    GPIO.setup(LED_MANUAL_OUT_ON , GPIO.OUT)
+    GPIO.setup(LED_MANUAL_OUT_OFF, GPIO.OUT)
+    GPIO.setup(LED_WIFI_LINK     , GPIO.OUT)
+
+    GPIO.setup(ADDR_0_PIN, GPIO.IN)
+    GPIO.setup(ADDR_1_PIN, GPIO.IN)
+    GPIO.setup(ADDR_2_PIN, GPIO.IN)
+
+    if GPIO.input(ADDR_2_PIN):
+        addr += 1 << 2
+    if GPIO.input(ADDR_1_PIN):
+        addr += 1 << 1
+    if GPIO.input(ADDR_0_PIN):
+        addr += 1
 
 context = zmq.Context()
-# socket = context.socket(zmq.REP)
-# socket.bind("tcp://*:5555")
 socket = context.socket(zmq.PAIR)
-# socket.connect("tcp://192.168.0.26:5555")
 socket.connect(f"tcp://192.168.0.26:550{addr}")
+time.sleep(0.5)
+count = 1
+while True:
+    event = socket.poll(timeout = 2000) # wait 2 seconds
+    if event == 0:
+        print(f'not connected. Try again {[count]}!!!')
+        if count == 3:
+            count = 1
+            socket.connect(f"tcp://192.168.0.26:550{addr}")
+            print('reconnect!!')
+        else:
+            count += 1
+
+        time.sleep(1)
+        continue
+    else:
+        print('connected!!!')
+        break
 
 command_dict = {
     'alive':        False,
@@ -68,8 +90,10 @@ command_dict = {
 def cleanup_gpio():
     GPIO.cleanup()
 
-atexit.register(cleanup_gpio)
+if RPI_ZERO:
+    atexit.register(cleanup_gpio)
 
+"""
 def alive_function():
     global command_dict
     command_dict['alive'] = True 
@@ -85,42 +109,58 @@ def start_periodic_task(interval_seconds):
     timer.start()
 
 start_periodic_task(1)
+"""
 
 while True:
+    event = socket.poll(timeout = 2000) # wait 2 seconds
+    if event == 0:
+        print('timeout!!!!!!!!!!!!!!!!!!')
+        # message['alive'] = False
+
+        if RPI_ZERO:
+            GPIO.output(LED_WIFI_LINK, GPIO.LOW)
+
+        time.sleep(1)
+        continue
+
+    else:
+        if RPI_ZERO:
+            GPIO.output(LED_WIFI_LINK, GPIO.HIGH)
+
     message = socket.recv_json()
     print(message)
-    # print('auto mode: ', message['auto_mode'])
-    # print('manual mode: ', message['manual_mode'])
 
     if message['alive'] == False:
         command_dict['alive'] = True
-        # socket.send_json(command_dict)
-        # continue
 
     if message['auto_mode']:
         # AUTO MODE -> relay on, led on
-        GPIO.output(AUTO_MODE_RELAY_PIN, GPIO.HIGH)
-        GPIO.output(LED_AUTO_MODE_ON, GPIO.HIGH)
-        # MANUAL MODE -> relay off, led off 
-        GPIO.output(MANUAL_MODE_RELAY_PIN, GPIO.LOW)
-        GPIO.output(LED_MANUAL_MODE_ON, GPIO.LOW)
+        if RPI_ZERO:
+            GPIO.output(AUTO_MODE_RELAY_PIN, GPIO.HIGH)
+            GPIO.output(LED_AUTO_MODE_ON, GPIO.HIGH)
+            # MANUAL MODE -> relay off, led off 
+            GPIO.output(MANUAL_MODE_RELAY_PIN, GPIO.LOW)
+            GPIO.output(LED_MANUAL_MODE_ON, GPIO.LOW)
         command_dict['auto_mode'] = ON
     else:
         # MANUAL MODE -> relay off, led off 
-        GPIO.output(AUTO_MODE_RELAY_PIN, GPIO.LOW)
-        GPIO.output(LED_AUTO_MODE_ON, GPIO.LOW)
-        GPIO.output(LED_MANUAL_MODE_ON, GPIO.HIGH)
+        if RPI_ZERO:
+            GPIO.output(AUTO_MODE_RELAY_PIN, GPIO.LOW)
+            GPIO.output(LED_AUTO_MODE_ON, GPIO.LOW)
+            GPIO.output(LED_MANUAL_MODE_ON, GPIO.HIGH)
         command_dict['auto_mode'] = OFF
 
         if message['manual_mode']:
-            GPIO.output(MANUAL_MODE_RELAY_PIN, GPIO.HIGH)
-            GPIO.output(LED_MANUAL_OUT_ON, GPIO.HIGH)
-            GPIO.output(LED_MANUAL_OUT_OFF, GPIO.LOW)
+            if RPI_ZERO:
+                GPIO.output(MANUAL_MODE_RELAY_PIN, GPIO.HIGH)
+                GPIO.output(LED_MANUAL_OUT_ON, GPIO.HIGH)
+                GPIO.output(LED_MANUAL_OUT_OFF, GPIO.LOW)
             command_dict['manual_mode'] = ON
         else:
-            GPIO.output(MANUAL_MODE_RELAY_PIN, GPIO.LOW)
-            GPIO.output(LED_MANUAL_OUT_ON, GPIO.LOW)
-            GPIO.output(LED_MANUAL_OUT_OFF, GPIO.HIGH)
+            if RPI_ZERO:
+                GPIO.output(MANUAL_MODE_RELAY_PIN, GPIO.LOW)
+                GPIO.output(LED_MANUAL_OUT_ON, GPIO.LOW)
+                GPIO.output(LED_MANUAL_OUT_OFF, GPIO.HIGH)
             command_dict['manual_mode'] = OFF
 
     #  Send reply back to client
